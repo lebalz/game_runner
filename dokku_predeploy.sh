4bbe4dd22e7733758f45a14e10073d240ee0d6fe
Balthasar Hofer <lebalz@outlook.com>
2021-01-04 19:45:34 +0100

    update

--- dokku_predeploy.sh
+++ dokku_predeploy.sh
@@ -8,5 +8,6 @@ apt-get update
 apt-get install -y inotify-tools
 touch /home/game_runner/run_state.log
 
-nohup /bin/bash /app/on_game_state_change.sh > /home/game_runner/run_state.log 2>&1
+# we can not start already here, since the container is not deployed yet...
+# nohup /bin/bash /app/on_game_state_change.sh > /home/game_runner/run_state.log 2>&1
 
