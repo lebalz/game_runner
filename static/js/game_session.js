8f4060e3b0fe080ee071a026f99814d0335a9277
Balthasar Hofer <lebalz@outlook.com>
2021-01-12 21:21:26 +0100

    report correct running game id

--- static/js/game_session.js
+++ static/js/game_session.js
@@ -4,12 +4,13 @@
  */
 const on_game_start = (event, gameId) => {
     const current_id = localStorage.getItem('running_game_id')
+    console.log('current game play id', current_id, 'new ID', gameId)
     if (current_id) {
         var xhttp = new XMLHttpRequest();
         xhttp.open("POST", "/terminate_game", true);
         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhttp.send(`id=${gameId}`);
+        xhttp.send(`id=${current_id}`);
     }
-    localStorage.setItem('running_game_id', current_id);
+    localStorage.setItem('running_game_id', gameId);
     return;
 }
